f4fe9539c864dfc6c36674585164312a99f62f27
yuuosh <yuu.oshiro@gmail.com>
2025-06-05 20:59:13 -0300

    Add files via upload

---
+++ ServerDBConnection.h
@@ -0,0 +1,26 @@
+#include <maridb/conncpp.hpp>
+
+#ifndef SERVERDBCONNECTION_H
+#define SERVERDBCONNECTION_H
+
+using namespace std;
+
+class ServerDBConnection final
+    {
+  private:
+    string serverPort = "3306";
+    string serverIP = "143.106.243.64";
+    string serverDatabase = "PooI_25_B09"; 
+    string serverUser = "PooI_25_B09;
+    string serverPassword = "YEdm2DlaRP";
+
+    sql::Driver *driver; = NULL;
+    sql::Connection *connection;
+
+   public:
+     ServerDBConnection();
+     virtual ~ServerDBConnection();
+     sql::Connection *getConnection() const;
+};
+
+#endif //SERVERDBCONNECTION_H
