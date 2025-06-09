ca1eceb38f1ff09a960c153e32759b6a1d1665eb
AndrezzaLz <andrezzaluize2@gmail.com>
2025-06-09 17:21:16 -0300

    v

--- main.cpp
+++ main.cpp
@@ -3,75 +3,121 @@
 #include "MovimentacaoDTO.hpp"
 #include "OracleDTO.hpp"
 #include "DAO_Memoria.hpp"
+#include "Menu.h"
 
 using namespace std;
 
 int main() {
     try {
-        cout << "--- Teste DTO: Carteira ---" << endl;
-        int idCarteira;
-        string nomeTitular, corretora;
+        vector<string> opcoesMenu = {
+            "Cadastrar Carteira",
+            "Cadastrar Movimentação",
+            "Cadastrar Cotação",
+            "Mostrar Movimentações",
+            "Sair"
+        };
 
-        cout << "ID da Carteira: ";
-        cin >> idCarteira;
-        cin.ignore();
+        Menu menu(opcoesMenu, "Menu Principal", "Escolha uma opção:");
+        menu.setRepeatList(true);
+        menu.setZeroForLastOpt(true); // Mostra "0 - Sair"
+        menu.setSymbol("=");
 
-        cout << "Nome do Titular: ";
-        getline(cin, nomeTitular);
-
-        cout << "Nome da Corretora: ";
-        getline(cin, corretora);
-
-        CarteiraDTO carteira(idCarteira, nomeTitular, corretora);
-        cout << "Carteira criada com sucesso!" << endl << endl;
-
-        cout << "--- Teste DTO: Movimentação ---" << endl;
-        int idMov;
-        string dataMov;
-        char tipo;
-        double quantidade;
-
-        cout << "ID da Movimentação: ";
-        cin >> idMov;
-
-        cout << "Data (YYYY-MM-DD): ";
-        cin >> dataMov;
-
-        cout << "Tipo ('C' para compra, 'V' para venda): ";
-        cin >> tipo;
-
-        cout << "Quantidade: ";
-        cin >> quantidade;
-
-        MovimentacaoDTO mov(idMov, idCarteira, dataMov, tipo, quantidade);
-        cout << "Movimentação criada com sucesso!" << endl << endl;
-
-        cout << "--- Teste DTO: Oracle (Cotação) ---" << endl;
-        string dataCot;
-        double cotacao;
-
-        cout << "Data (YYYY-MM-DD): ";
-        cin >> dataCot;
-
-        cout << "Cotação da moeda: ";
-        cin >> cotacao;
-
-        OracleDTO oracle(dataCot, cotacao);
-        cout << "Cotação registrada com sucesso!" << endl << endl;
-
-        cout << "--- Teste DAO em Memória ---" << endl;
+        // Objetos persistentes
+        CarteiraDTO carteira = { 1, "Jefferson", "XP"};
         DAO_Memoria dao;
-        dao.create(mov);  // adiciona a movimentação criada
 
-        cout << "Movimentações armazenadas:" << endl;
-        for (const auto& m : dao.listAll()) {
-            cout << "ID: " << m.id << ", Carteira: " << m.idCarteira
-                 << ", Data: " << m.data << ", Tipo: " << m.tipoOperacao
-                 << ", Quantidade: " << m.quantidade << endl;
-        }
+        int escolha;
+        do {
+            escolha = menu.getChoice();
+
+            switch (escolha) {
+                case 1: {
+                    cout << "--- Cadastrar Carteira ---" << endl;
+                    int idCarteira;
+                    string nomeTitular, corretora;
+
+                    cout << "ID da Carteira: ";
+                    cin >> idCarteira;
+                    cin.ignore();
+
+                    cout << "Nome do Titular: ";
+                    getline(cin, nomeTitular);
+
+                    cout << "Nome da Corretora: ";
+                    getline(cin, corretora);
+
+                    carteira = CarteiraDTO(idCarteira, nomeTitular, corretora);
+                    cout << "Carteira criada com sucesso!" << endl << endl;
+                    break;
+                }
+
+                case 2: {
+                    cout << "--- Cadastrar Movimentação ---" << endl;
+                    int idMov;
+                    string dataMov;
+                    char tipo;
+                    double quantidade;
+
+                    cout << "ID da Movimentação: ";
+                    cin >> idMov;
+
+                    cout << "Data (YYYY-MM-DD): ";
+                    cin >> dataMov;
+
+                    cout << "Tipo ('C' para compra, 'V' para venda): ";
+                    cin >> tipo;
+
+                    cout << "Quantidade: ";
+                    cin >> quantidade;
+
+                    MovimentacaoDTO mov(idMov, carteira.id, dataMov, tipo, quantidade);
+                    dao.create(mov);
+
+                    cout << "Movimentação criada com sucesso!" << endl << endl;
+                    break;
+                }
+
+                case 3: {
+                    cout << "--- Cadastrar Cotação (Oracle) ---" << endl;
+                    string dataCot;
+                    double cotacao;
+
+                    cout << "Data (YYYY-MM-DD): ";
+                    cin >> dataCot;
+
+                    cout << "Cotação da moeda: ";
+                    cin >> cotacao;
+
+                    OracleDTO oracle(dataCot, cotacao);
+                    cout << "Cotação registrada com sucesso!" << endl << endl;
+                    break;
+                }
+
+                case 4: {
+                    cout << "--- Movimentações Armazenadas ---" << endl;
+                    for (const auto& m : dao.listAll()) {
+                        cout << "ID: " << m.id
+                             << ", Carteira: " << m.idCarteira
+                             << ", Data: " << m.data
+                             << ", Tipo: " << m.tipoOperacao
+                             << ", Quantidade: " << m.quantidade << endl;
+                    }
+                    cout << endl;
+                    break;
+                }
+
+                case 0:
+                    cout << "Encerrando o programa." << endl;
+                    break;
+
+                default:
+                    cout << "Opção inválida!" << endl;
+            }
+
+        } while (escolha != 0);
 
     } catch (const exception& e) {
-        cerr << "Erro ao criar DTO ou usar DAO: " << e.what() << endl;
+        cerr << "Erro: " << e.what() << endl;
     }
 
     return 0;
