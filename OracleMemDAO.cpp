6661c469221f09f2d1b6b8ccc6e6b5ff3fc6822b
Henrique-Saito <henriquesaito64@gmail.com>
2025-06-17 19:46:17 -0300

    OracleMemDAO.cpp

--- OracleMemDAO.cpp
+++ OracleMemDAO.cpp
@@ -1,24 +1,31 @@
 #include "OracleMemDAO.hpp"
 #include "MemoryDBConnection.hpp"
 #include "OracleDTO.hpp"
+#include "RegistroNaoEncontradoException.hpp" // ADICIONADO
 #include <iostream>
 #include <algorithm>
+#include <string> // ADICIONADO
 
 OracleMemDAO::OracleMemDAO(MemoryDBConnection* memoryDBConnection) : memoryDBConnection(memoryDBConnection) {}
 
 OracleMemDAO::~OracleMemDAO() {}
 
+// MÉTODO MODIFICADO
 OracleDTO* OracleMemDAO::getQuoteByDate(Date date) {
     auto it = memoryDBConnection->getOracleMap().find(date.getIsoFormat());
+
     if (it != memoryDBConnection->getOracleMap().end()) {
         return it->second;
     }
-    return nullptr; // Retorna nullptr se a data não for encontrada
+    
+    // Se não encontrou, lança a exceção
+    throw RegistroNaoEncontradoException("Cotacao para a data " + date.getIsoFormat() + " nao encontrada.");
 }
 
 void OracleMemDAO::saveQuote(OracleDTO* quote) {
-    string dateStr = quote->getDate().getIsoFormat();
+    std::string dateStr = quote->getDate().getIsoFormat();
     auto it = memoryDBConnection->getOracleMap().find(dateStr);
+    
     if (it != memoryDBConnection->getOracleMap().end()) {
         delete it->second;
         it->second = quote;
